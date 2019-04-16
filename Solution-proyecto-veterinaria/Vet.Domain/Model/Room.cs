c3dab13775189c458f26b53ca97ceb44d423ac4f
Leandro <castroleandrof@gmail.com>
2019-04-15 21:38:04 -0300

    1

---
+++ Solution-proyecto-veterinaria/Vet.Domain/Model/Room.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vet.Domain.Model
+{
+    public class Room
+    {
+        public String name { get; set; }
+    }
+}
